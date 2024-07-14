aef6518a320f32768ac6252eec5e81e6c8fd6ded
Ygrik <test@Dubrovin02-2.local>
2024-07-14 22:15:13 +0300

    sql

--- sql.sql
+++ sql.sql
@@ -29,20 +29,22 @@ FROM
 GROUP BY
     c.issue_date, cat.name;
 
+drop materialized view if exists small.mv_shelve_count cascade;
+CREATE MATERIALIZED VIEW small.mv_shelve_count AS
+    SELECT c.name AS category_name, SUM(pos.product_count) AS shelve_count
+    FROM products_on_shelves pos
+        JOIN product_categories pc ON pos.product_id = pc.product_id
+        JOIN categories c ON pc.category_id = c.category_id
+    GROUP BY c.name;
 
-drop materialized view if exists small.mv_product_summary cascade;
-CREATE MATERIALIZED VIEW small.mv_product_summary AS
-SELECT
-    c.issue_date AS date,
-    cat.name AS category_name,
-    SUM(pcp.product_count) AS sum_products_issue_date
-FROM
-    checks c
-    LEFT JOIN product_check_positions pcp ON pcp.check_id = c.check_id
-    LEFT JOIN product_categories pc ON pcp.product_id = pc.product_id
-    LEFT JOIN categories cat ON pc.category_id = cat.category_id
-GROUP BY
-    c.issue_date, cat.name;
+
+drop materialized view if exists prod.mv_shelve_count cascade;
+CREATE MATERIALIZED VIEW prod.mv_shelve_count AS
+SELECT c.name AS category_name, SUM(pos.product_count) AS shelve_count
+FROM products_on_shelves pos
+         JOIN product_categories pc ON pos.product_id = pc.product_id
+         JOIN categories c ON pc.category_id = c.category_id
+GROUP BY c.name;
 
 drop materialized view if exists prod.mv_external_supplies cascade;
 CREATE MATERIALIZED VIEW prod.mv_external_supplies AS
@@ -76,16 +78,4 @@ GROUP BY
     c.issue_date, cat.name;
 
 
-drop materialized view if exists prod.mv_product_summary cascade;
-CREATE MATERIALIZED VIEW prod.mv_product_summary AS
-SELECT
-    c.issue_date AS date,
-    cat.name AS category_name,
-    SUM(pcp.product_count) AS sum_products_issue_date
-FROM
-    checks c
-    LEFT JOIN product_check_positions pcp ON pcp.check_id = c.check_id
-    LEFT JOIN product_categories pc ON pcp.product_id = pc.product_id
-    LEFT JOIN categories cat ON pc.category_id = cat.category_id
-GROUP BY
-    c.issue_date, cat.name;
+
