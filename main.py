e40a9d69c83ece8d469f0bb9c4ae735aaf07b975
Ygrik <test@Dubrovin02-2.local>
2024-07-14 12:29:07 +0300

    init

--- main.py
+++ main.py
@@ -2,17 +2,24 @@ import streamlit as st
 import pandas as pd
 import before_database
 from draw import draw_graph
-import streamlit
 
 import database
+before_database.init_data_small()
+before_database.init_date_prod()
+
+events, shelves_count, categories = before_database.take_data('prod')
+graph_data_prod, index_prod = draw_graph(events, shelves_count, categories)
+
+events, shelves_count, categories = before_database.take_data('small')
+graph_data_small, index_small = draw_graph(events, shelves_count, categories)
 
 schema = st.selectbox('Select schema', ('small', 'prod'), key='schema_selector')
 
 if schema == 'small':
-    data = pd.DataFrame(data=streamlit.graph_data_small, index=streamlit.index_small)
+    data = pd.DataFrame(data=graph_data_small, index=index_small)
     st.area_chart(data)
 else:
-    data = pd.DataFrame(data=streamlit.graph_data_prod, index=streamlit.index_prod)
+    data = pd.DataFrame(data=graph_data_prod, index=index_prod)
     st.area_chart(data)
 
 
