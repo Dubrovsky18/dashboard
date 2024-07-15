0ddcd2e53036cd51f121549d2c3e7362b57b0b8d
Ygrik <test@Dubrovin02-2.local>
2024-07-15 09:55:19 +0300

    modification

--- bash.sh
+++ bash.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
 
 cat <<EOT >> /etc/postgresql/16/main/conf.d/perf.conf
-shared_buffers = 1GB
-max_connections = 1024
-effective_cache_size = 512MB
+shared_buffers = 256MB
+max_connections = 100
+effective_cache_size = 256MB
 
-work_mem = 254MB
+work_mem = 256MB
 max_worker_processes = 4
 max_parallel_workers = 4
 max_parallel_maintenance_workers = 4
 max_parallel_workers_per_gather = 4
-maintenance_work_mem = 1GB
 
 checkpoint_completion_target = 0.9
 checkpoint_timeout = 20min
 default_statistics_target = 300
 random_page_cost = 1.0
-effective_io_concurrency = 500
+effective_io_concurrency = 300
 synchronous_commit = off
 huge_pages = off
-wal_buffers = 64MB
-temp_buffers = 64MB
-min_wal_size = 256MB
-max_wal_size = 1GB
 EOT
 systemctl restart postgresql
